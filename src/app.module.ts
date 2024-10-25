b668d7676f66ef3c443170661cd847aaaec948fa
Keimer10 <134401195+Keimer10@users.noreply.github.com>
2024-10-25 16:04:16 -0500

    conexion base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { ContenidosModule } from './contenidos/contenidos.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { ComentariosModule } from './comentarios/comentarios.module';
 import { OvasModule } from './ovas/ovas.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ContenidosModule, UsuariosModule, ComentariosModule, OvasModule],
+  imports: [ContenidosModule, UsuariosModule, ComentariosModule, OvasModule,
+      MongooseModule.forRoot('mongodb+srv://kmunozmora:ywKS8RXKur9sKb0n@cluster0.yuzpj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')],
   controllers: [AppController],
   providers: [AppService],
 })
