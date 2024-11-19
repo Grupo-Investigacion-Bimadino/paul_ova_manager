588ec0a18d6d5f05fb7ddba29b01d58c84816bd1
Keimer10 <134401195+Keimer10@users.noreply.github.com>
2024-11-19 08:13:12 -0500

    Se agregaron atributos en COMENTARIOS

--- src/comentarios/comentarios.service.ts
+++ src/comentarios/comentarios.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateComentarioDto } from './dto/create-comentario.dto';
 import { UpdateComentarioDto } from './dto/update-comentario.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Comentarios } from './schemas/comentarios.schema';
 
 @Injectable()
 export class ComentariosService {
-  create(createComentarioDto: CreateComentarioDto) {
-    return 'This action adds a new comentario';
+  constructor(@InjectModel(Comentarios.name) private comentariosModel: Model<Comentarios>) {}
+  async create(createComentarioDto: CreateComentarioDto) {
+    const createdComentarios = new this.comentariosModel(createComentarioDto);
+    const result = await createdComentarios.save();
+    return result;
   }
 
   findAll() {
