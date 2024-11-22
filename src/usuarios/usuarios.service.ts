6a81042e0469581652b6801e73d6b6bc62f4a424
Keimer10 <134401195+Keimer10@users.noreply.github.com>
2024-11-22 13:36:41 -0500

    se modificó en el decorardor INJECTABLE

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Usuarios } from './schemas/usuarios.schema';
 
 @Injectable()
 export class UsuariosService {
-  create(createUsuarioDto: CreateUsuarioDto) {
-    return 'This action adds a new usuario';
+  constructor(@InjectModel(Usuarios.name) private usuariosModel: Model<Usuarios>) {}
+  async create(createUsuarioDto: CreateUsuarioDto) {
+    const createdUsuarios = new this.usuariosModel(createUsuarioDto);
+    const result = await createdUsuarios.save();
+    return result;
   }
 
   findAll() {
