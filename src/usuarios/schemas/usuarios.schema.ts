ae6b3469f3f597083b560be873cba0face12d3b4
Keimer10 <134401195+Keimer10@users.noreply.github.com>
2024-11-29 10:14:21 -0500

    se añadieron los atributos de usuarios en el respectivo schema

--- src/usuarios/schemas/usuarios.schema.ts
+++ src/usuarios/schemas/usuarios.schema.ts
@@ -5,6 +5,33 @@ import { Document } from 'mongoose';
 export class Usuarios extends Document {
   @Prop()
   id_contenidos: [];
+
+  @Prop()
+    nombre_usuario: string;
+
+  @Prop({ unique: [true, 'Email already exists'] })
+    correo: string;
+
+  @Prop()
+    contraseña: string;
+
+  @Prop({default: 'guest'})
+    rol: string;
+
+  @Prop()
+    estado: string;
+
+  @Prop()
+    creado_en: Date;
+
+  @Prop()
+    imagen: string;
+
+  @Prop()
+    actualizado_en: Date;
+    
 }
+
+
 
 export const UsuariosSchema = SchemaFactory.createForClass(Usuarios);
